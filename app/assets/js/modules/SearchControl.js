56a6e49471d48acf5486e6f24840613128582225
TrungThanh Nguyen <trungthanh.nguyen@ogilvy.com>
2018-03-09 18:19:20 +0700

    search layout

--- app/assets/js/modules/SearchControl.js
+++ app/assets/js/modules/SearchControl.js
@@ -3,26 +3,27 @@ export default class SearchControl {
 
 		$('[data-toggle="tooltip"]').tooltip();
 		$('[data-toggle="collapse"]').collapse();
-	
+
 
 		$(".clear-filter").on('click', function(){
 			$('.filter-wrapper input[type=checkbox]').each(function(){ 
 				this.checked = false; 
 			});
 		});
-		
-    $(el).jplist({
-      itemsBox: '.list-box',
-      itemPath: '.thumb-item',
-      panelPath: '.jplist-panel',
-      storage: 'localstorage',
-      storageName: 'mixed-controls-local-storage',
-      effect: 'fade',
-      redrawCallback: function(collection, $dataview, statuses){
-        $(".list-box").find(".slider-wrapper").each(function(){
-          $(this).slick('refresh');
-        });
-      }
-    });
-	}
+
+
+		$(el).jplist({
+			itemsBox: '.list-box',
+			itemPath: '.thumb-item',
+			panelPath: '.jplist-panel',
+			storage: 'localstorage',
+			storageName: 'mixed-controls-local-storage',
+			effect: 'fade',
+			redrawCallback: function(collection, $dataview, statuses){
+				$(".list-box").find(".slider-wrapper").each(function(){
+					$(this).slick('refresh');
+				});
+			}
+		});
+  }
 }
